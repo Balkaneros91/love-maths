61011089f64be6f6c1bfa9d413d3c7e1ffc30c63
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2022-11-14 10:18:49 +0000

    Add code for rungame calculateCorrectAnswer and division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,8 @@ function runGame(gameType) {
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
+    let num3 = Math.trunc(Math.random() * 100) + 1;
+	let num4 = Math.trunc(Math.random() * 5) + 1;
 
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
@@ -45,7 +47,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === 'subtract') {
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division")  {
+	    displayDivisionQuestion(num3, num4);
+	} else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`
     }
@@ -89,7 +93,9 @@ function calculateCorrectAnswer() {
             return [operand1 - operand2, 'subtract'];
     } else if (operator === 'x') {
             return [operand1 * operand2, 'multiply'];
-    } else {
+    } else if (operator === "/") {
+	    return [operand1 / operand2, "division"];
+	} else {
             alert(`Unimplemented operator ${operator}`);
             throw `Unimplemented operator ${operator}. Aborting!`
         }
@@ -156,6 +162,12 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-/* function displayDivisionQuestion() {
-
-} */
+/* function displayDivisionQuestion() {} */
+// Add your division question here
+function displayDivisionQuestion(operand1, operand2) {
+    
+    document.getElementById("operand1").textContent = (operand1 * operand2);
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+    
+}
